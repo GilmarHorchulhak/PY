6ee47c3a711178d0a0be40af9a5830358d824c8b
Gilmar Horchulhak <89936389+GilmarHorchulhak@users.noreply.github.com>
2024-02-22 18:40:08 +0000

    1

--- 1/app2.py
+++ 1/app2.py
@@ -1,5 +1,11 @@
 
-nome=input("Qual o seu nome?")
-idade=input("qual a sua idade?")
+numero1=int(input('Digite um número: '))#a variável numero1 armazena o valor digitado na fomra de um numero inteiro.
 
-print(f"O seu nome é {nome} e sua idade é{idade}")
+numero2=int(input('Digite outro número: '))
+operacao = input('Qual a operação?')
+if operacao=='+':
+    soma = numero1 + numero2
+elif operacao=='-':
+    soma= numero1 - numero2
+    
+print(soma)
